4911884c62c27e470dd3a5b38b9a6171cf191c2d
kyriakos-katsigarakis <k.katsigarakis@ucl.ac.uk>
2023-03-02 20:09:16 +0200

    ...

--- datapoint-mapper/src/main/java/uk/ac/ucl/iede/dpm/config/DatapointMapper.java
+++ datapoint-mapper/src/main/java/uk/ac/ucl/iede/dpm/config/DatapointMapper.java
@@ -2,21 +2,18 @@ package uk.ac.ucl.iede.dpm.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.ac.ucl.iede.dpm.controller.MainController;
 
 @Configuration
 @SpringBootApplication
-@EntityScan("uk.ac.ucl.iede.dpm.dom")
-@EnableJpaRepositories({"uk.ac.ucl.iede.dpm.dom","uk.ac.ucl.iede.dpm.dao"})
+//@EntityScan("uk.ac.ucl.iede.dpm.dom")
+//@EnableJpaRepositories({"uk.ac.ucl.iede.dpm.dom","uk.ac.ucl.iede.dpm.dao"})
 @ComponentScan(basePackageClasses = {MainController.class})
 public class DatapointMapper {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatapointMapper.class, args);
 	}
-
 }
