cf854870fe63f316f4204426b1f7183de7a33a0c
kyriakos-katsigarakis <k.katsigarakis@ucl.ac.uk>
2023-03-02 10:55:12 +0200

    ...

---
+++ datapoint-mapper/src/main/java/uk/ac/ucl/iede/DatapointMapper.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.iede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapointMapper {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapointMapper.class, args);
+	}
+
+}
